4dc25ae25e084b7213098b5d90c62a7edb6b5ec7
Arghya-7 <86887696+Arghya-7@users.noreply.github.com>
2022-03-12 17:51:13 +0530

    Update Emergency-Ambulance-Service.py
    In this version we have create Admin table to secure the database.
    Use Admin ph. no = 1234567891
    Use Admin password = 123

--- Emergency-Ambulance-Service.py
+++ Emergency-Ambulance-Service.py
@@ -32,8 +32,160 @@ def showStatus():
 #mycursor.execute("CREATE TABLE service(name VARCHAR(255),mobile VARCHAR(20),area VARCHAR(255),mode VARCHAR(2))")
 
 #Admin Table
-##mycursor.execute("CREATE TABLE admin(mobile VARCHAR(20),password VARCHAR(20),PRIMARY KEY (mobile))")
-class Creator:
+#mycursor.execute("CREATE TABLE admin(mobile VARCHAR(20),password VARCHAR(20),PRIMARY KEY (mobile))")
+
+class User:
+    def usershowStatus(self):
+        showStatus()
+
+    def seeLocationWise(self):
+        place = input("Enter the location name:").upper()
+        pin = input("Enter the pincode without space:")
+        try:
+            sql = "SELECT name,mobile,area,pincode FROM service WHERE mode='1' AND area=" + "'" + place + "'" + " AND " "pincode=" + "'" + pin + "'"
+            mycursor.execute(sql)
+            result = mycursor.fetchall()
+            print(f"{'Name':<12}{'Phone Number':>12}{'Area':>12}{'Pincode':>12}")
+            for i in result:
+                print(f"{i[0]:<12}{i[1]:>12}{i[2]:>12}{i[3]:>12}")
+            mydb.commit()
+        except Exception:
+            print("Sorry no ambulance there")
+            print("you can check by Pincode also")
+
+    def seePincodeWise(self):
+        pin = input("Enter the pincode without space:")
+        try:
+            sql = "SELECT name,mobile,area,pincode FROM service WHERE mode='1' AND pincode=" + "'" + pin + "'"
+            mycursor.execute(sql)
+            result = mycursor.fetchall()
+            print(f"{'Name':<12}{'Phone Number':>12}{'Area':>12}{'Pincode':>12}")
+            for i in result:
+                print(f"{i[0]:<12}{i[1]:>12}{i[2]:>12}{i[3]:>12}")
+            mydb.commit()
+        except Exception:
+            print("Sorry no ambulance there")
+            print("you can check by Pincode also")
+
+    def control(self):
+        print("1 for available Ambulance of database")
+        print("2 for check Location and pin wise Ambulance")
+        print("3 for pincode wise ambulance")
+        ch=int(input("Enter your choice:"))
+        if ch==1:
+            self.usershowStatus()
+        elif ch==2:
+            self.seeLocationWise()
+        elif ch==3:
+            self.seePincodeWise()
+        else:
+            print("Wrong choice")
+
+class Driver:
+    def add(self):
+        try:
+            name=input("Enter your name:").upper()
+            while True:
+                mobile = input("Enter 10 digit mobile number:")
+                if len(mobile) != 10:
+                    print("Oops!Your mobile number is not 10 digit")
+                else:
+                    break
+            area=input("Enter the area:").upper()
+            mode="1"
+            pincode = input("Enter the pincode:")
+            sql="INSERT INTO service(name ,mobile,area,mode,pincode) VALUES(%s,%s,%s,%s,%s);"
+            val=(name,mobile,area,mode,pincode)
+            mycursor.execute(sql,val)
+            mydb.commit()
+        except Exception:
+            print("You are already registered or some internal issue occurs")
+            print("Contact admin")
+    def deleteRecord(self):
+        mobile = input("Enter 10 digit mobile number:")
+        while True:
+            if len(mobile) != 10:
+                mobile = input("Re-enter mobile number:")
+            else:
+                break
+        sql="DELETE FROM service WHERE mobile= " + "'"+ mobile + "'"
+        mycursor.execute(sql)
+        mydb.commit()
+    def driverShowStatus(self):
+        showStatus()
+    def makeOffline(self):
+        mobile = input("Enter 10 digit mobile number of the Driver:")
+        while True:
+            if len(mobile) != 10:
+                mobile = input("Re-enter mobile number:")
+            else:
+                break
+        sql="UPDATE service SET mode=" + "'" + str(0) + "'"  + "WHERE mobile="+ "'" + mobile + "';"
+        mycursor.execute(sql)
+        mydb.commit()
+    def makeOnline(self):
+        mobile = input("Enter 10 digit mobile number of the Driver:")
+        while True:
+            if len(mobile) != 10:
+                mobile = input("Re-enter mobile number:")
+            else:
+                break
+        sql="UPDATE service SET mode=" + "'" + str(1) +"'" + "WHERE mobile="+ "'" + mobile + "'"
+        mycursor.execute(sql)
+        mydb.commit()
+
+    def changeLocation(self):
+        mobile = input("Enter 10 digit mobile number of the Driver:")
+        while True:
+            if len(mobile) != 10:
+                mobile = input("Re-enter mobile number:")
+            else:
+                break
+        place = input("Enter the location:").upper()
+        pincode = input("Enter the pin:")
+        sql = "UPDATE service SET area='" + place + "', pincode='" + pincode + "'" + " WHERE mobile=" + "'" + mobile + "'"
+        mycursor.execute(sql)
+        mydb.commit()
+
+
+    def selfVisualization(self):
+        mobile = input("Enter 10 digit mobile number of the Driver:")
+        while True:
+            if len(mobile) != 10:
+                mobile = input("Re-enter mobile number:")
+            else:
+                break
+        sql = "SELECT name,mobile,area,pincode FROM service WHERE mobile='" + mobile + "'"
+        mycursor.execute(sql)
+        result = mycursor.fetchall()
+        print(f"{'Name':<12}{'Phone Number':>12}{'Area':>12}{'Pincode':>12}")
+        for i in result:
+            print(f"{i[0]:<12}{i[1]:>12}{i[2]:>12}{i[3]:>12}")
+
+    def control(self):
+        print("1 for Sign-up in database")
+        print("2 delete your record permanently")
+        print("3 for showing available drivers in database")
+        print("4 for going offline")
+        print("5 for going online")
+        print("6 for change location")
+        print("7 for checking personal details")
+        ch=int(input("Enter the choice:"))
+        if ch == 1:
+            self.add()
+        elif ch==2:
+            self.deleteRecord()
+        elif ch==3:
+            self.driverShowStatus()
+        elif ch==4:
+            self.makeOffline()
+        elif ch==5:
+            self.makeOnline()
+        elif ch==6:
+            self.changeLocation()
+        elif ch==7:
+            self.selfVisualization()
+class Creator(Driver,User):
     def redefineDatabase(self):
         #mycursor.execute("DROP TABLE service")
         mycursor.execute("DROP TABLE IF EXISTS service")
@@ -99,6 +251,61 @@ class Creator:
         except Exception:
             pass
 
+    def insertIntoAdminTable(self):
+        mobile = input("Enter 10 digit mobile number of the Admin:")
+        while True:
+            if len(mobile) != 10:
+                mobile = input("Re-enter mobile number:")
+            else:
+                break
+
+        password = "123"
+        sql = "INSERT INTO admin(mobile,password) VALUES (%s,%s)"
+        val = (mobile, password)
+        mycursor.execute(sql, val)
+        mydb.commit()
+
+    def checkPassword(self,mobile,password):
+        sql = "SELECT * FROM admin WHERE mobile = '" + mobile + "' AND password='" + password + "'"
+        mycursor.execute(sql)
+        result = mycursor.fetchall()
+        if len(result) == 0:
+            print("Mistakes occur in Phone number or Password")
+            return False
+        else:
+            return True
+
+    def updateAdminPassword(self):
+        mobile = input("Enter 10 digit mobile number of the Admin:")
+        while True:
+            if len(mobile) != 10:
+                mobile = input("Re-enter mobile number:")
+            else:
+                break
+        oldPassword = input("Enter old password:")
+        newPassword = input("Enter new Password:")
+        if self.checkPassword(mobile,oldPassword) is True:
+            sql = "UPDATE admin SET password = '" + newPassword + "' " + "WHERE mobile='" + mobile + "' AND password='" + oldPassword + "'"
+            mycursor.execute(sql)
+            mydb.commit()
+
+    def deleteAdmin(self):
+        mobile = input("Enter 10 digit mobile number of the Admin:")
+        while True:
+            if len(mobile) != 10:
+                mobile = input("Re-enter mobile number:")
+            else:
+                break
+        password = input("Enter your password:")
+        sql = "DELETE FROM admin WHERE mobile='" + mobile + "' AND password='" + password + "'"
+        mycursor.execute(sql)
+        mydb.commit()
+
+    def showAdminTable(self):
+        mycursor.execute("SELECT * FROM admin")
+        result = mycursor.fetchall()
+        for i in result:
+            print(i)
     def control(self):
         while True:
             print("0 for exit")
@@ -107,6 +314,11 @@ class Creator:
             print("3 for making  sign-in a driver")
             print("4 for delete particular driver records")
             print("5 to find a driver")
+            print("6 for getting Driver-Class-Control")
+            print("7 for getting User-Class-Control")
+            print("8 for insert a person into admin table")
+            print("9 for change admin password")
+            print("10 for delete admin")
             ch=int(input("Enter the choice:"))
             if ch==0:
                 break
@@ -120,172 +332,39 @@ class Creator:
                 self.delete()
             elif ch==5:
                 self.findDriver()
+            elif ch==6:
+                Driver.control(self)
+            elif ch==7:
+                User.control(self)
+            elif ch==8:
+                self.insertIntoAdminTable()
+            elif ch==9:
+                self.checkPassword()
+            elif ch==10:
+                self.deleteAdmin()
+                break
             else:
                 print("You have entered wrong choice")
 
-class User:
-    def usershowStatus(self):
-        showStatus()
 
-    def seeLocationWise(self):
-        place = input("Enter the location name:").upper()
-        pin = input("Enter the pincode without space:")
-        try:
-            sql = "SELECT name,mobile,area,pincode FROM service WHERE mode='1' AND area=" + "'" + place + "'" + " AND " "pincode=" + "'" + pin + "'"
-            mycursor.execute(sql)
-            result = mycursor.fetchall()
-            print(f"{'Name':<12}{'Phone Number':>12}{'Area':>12}{'Pincode':>12}")
-            for i in result:
-                print(f"{i[0]:<12}{i[1]:>12}{i[2]:>12}{i[3]:>12}")
-            mydb.commit()
-        except Exception:
-            print("Sorry no ambulance there")
-            print("you can check by Pincode also")
-
-    def seePincodeWise(self):
-        pin = input("Enter the pincode without space:")
-        try:
-            sql = "SELECT name,mobile,area,pincode FROM service WHERE mode='1' AND pincode=" + "'" + pin + "'"
-            mycursor.execute(sql)
-            result = mycursor.fetchall()
-            print(f"{'Name':<12}{'Phone Number':>12}{'Area':>12}{'Pincode':>12}")
-            for i in result:
-                print(f"{i[0]:<12}{i[1]:>12}{i[2]:>12}{i[3]:>12}")
-            mydb.commit()
-        except Exception:
-            print("Sorry no ambulance there")
-            print("you can check by Pincode also")
-
-    def control(self):
-        print("1 for available Ambulance of database")
-        print("2 for check Location and pin wise Ambulance")
-        print("3 for pincode ambulance")
-        ch=int(input("Enter your choice:"))
-        if ch==1:
-            self.usershowStatus()
-        elif ch==2:
-            self.seeLocationWise()
-        elif ch==3:
-            self.seePincodeWise()
-        else:
-            print("Wrong choice")
-class Driver:
-    def add(self):
-        try:
-            name=input("Enter your name:").upper()
-            while True:
-                mobile = input("Enter 10 digit mobile number:")
-                if len(mobile) != 10:
-                    print("Oops!Your mobile number is not 10 digit")
-                else:
-                    break
-            area=input("Enter the area:").upper()
-            mode="1"
-            pincode = input("Enter the pincode:")
-            sql="INSERT INTO service(name ,mobile,area,mode,pincode) VALUES(%s,%s,%s,%s,%s);"
-            val=(name,mobile,area,mode,pincode)
-            mycursor.execute(sql,val)
-            mydb.commit()
-        except Exception:
-            print("You are already registered or some internal issue occurs")
-            print("Contact admin")
-    def deleteRecord(self):
-        mobile = input("Enter 10 digit mobile number:")
-        while True:
-            if len(mobile) != 10:
-                mobile = input("Re-enter mobile number:")
-            else:
-                break
-        sql="DELETE FROM service WHERE mobile= " + "'"+ mobile + "'"
-        mycursor.execute(sql)
-        mydb.commit()
-    def driverShowStatus(self):
-        showStatus()
-    def makeOffline(self):
-        mobile = input("Enter 10 digit mobile number of the Driver:")
-        while True:
-            if len(mobile) != 10:
-                mobile = input("Re-enter mobile number:")
-            else:
-                break
-        sql="UPDATE service SET mode=" + "'" + str(0) + "'"  + "WHERE mobile="+ "'" + mobile + "';"
-        mycursor.execute(sql)
-        mydb.commit()
-    def makeOnline(self):
-        mobile = input("Enter 10 digit mobile number of the Driver:")
-        while True:
-            if len(mobile) != 10:
-                mobile = input("Re-enter mobile number:")
-            else:
-                break
-        sql="UPDATE service SET mode=" + "'" + str(1) +"'" + "WHERE mobile="+ "'" + mobile + "'"
-        mycursor.execute(sql)
-        mydb.commit()
-
-    def changeLocation(self):
-        mobile = input("Enter 10 digit mobile number of the Driver:")
-        while True:
-            if len(mobile) != 10:
-                mobile = input("Re-enter mobile number:")
-            else:
-                break
-        place = input("Enter the location:").upper()
-        sql = "UPDATE service SET area=" + "'" + place + "'" + "WHERE mobile=" + "'" + mobile + "'"
-        mycursor.execute(sql)
-        mydb.commit()
-
-    def selfVisualization(self):
-        mobile = input("Enter 10 digit mobile number of the Driver:")
-        while True:
-            if len(mobile) != 10:
-                mobile = input("Re-enter mobile number:")
-            else:
-                break
-        sql = "SELECT name,mobile,area,pincode FROM service WHERE mobile='" + mobile + "'"
-        mycursor.execute(sql)
-        result = mycursor.fetchall()
-        print(f"{'Name':<12}{'Phone Number':>12}{'Area':>12}{'Pincode':>12}")
-        for i in result:
-            print(f"{i[0]:<12}{i[1]:>12}{i[2]:>12}{i[3]:>12}")
-
-    def control(self):
-        print("1 for Sign-in in database")
-        print("2 delete your record permanently")
-        print("3 for showing available drivers in database")
-        print("4 for going offline")
-        print("5 for going online")
-        print("6 for change location")
-        print("7 for checking personal details")
-        ch=int(input("Enter the choice:"))
-        if ch == 1:
-            self.add()
-        elif ch==2:
-            self.deleteRecord()
-        elif ch==3:
-            self.driverShowStatus()
-        elif ch==4:
-            self.makeOffline()
-        elif ch==5:
-            self.makeOnline()
-        elif ch==6:
-            self.changeLocation()
-        elif ch==7:
-            self.selfVisualization()
 def main():
     print("1 for Creator")
     print("2 for User")
     print("3 for Drivers")
     ch=int(input("Enter the choice:"))
     if ch == 1:
-        name=input("Enter the Username:")
+        mobile = input("Enter 10 digit mobile number of the Admin:")
+        while True:
+            if len(mobile) != 10:
+                mobile = input("Re-enter mobile number:")
+            else:
+                break
         password=input("Enter the password:")
-        if name== adminName and password==adminPassword:
+        obj1=Creator()
+        if obj1.checkPassword(mobile,password) is True:
             print("Welcome Admin")
             obj = Creator()
             obj.control()
-        else:
-            print("Oops!You Have Entered Wrong Password")
-            print("Please Login Again")
             sys.exit(0)
     elif ch == 2:
         obj= User()
