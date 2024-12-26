d670606ef0ad6ebb6a4b853e8a60fac7ec837b68
Arghya Dey <arghyadey639@gmail.com>
2024-12-26 19:52:32 +0530

    added logger in project

--- src/main/java/com/Arghya/EmergencyAmbulaceService/EmergencyAmbulaceServiceApplication.java
+++ src/main/java/com/Arghya/EmergencyAmbulaceService/EmergencyAmbulaceServiceApplication.java
@@ -1,5 +1,7 @@
 package com.Arghya.EmergencyAmbulaceService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -7,10 +9,11 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 //@ComponentScan ({"com.Arghya.EmergecyAmbulanceService.service", "com.Arghya.EmergecyAmbulanceService.service.UserServices"})
 public class EmergencyAmbulaceServiceApplication {
-
+	public static final Logger logger = LoggerFactory.getLogger(EmergencyAmbulaceServiceApplication.class);
 	public static void main(String[] args) 
 	{	
 		SpringApplication.run(EmergencyAmbulaceServiceApplication.class, args);
+		logger.info("Application started successfully");
 	}
 
 }
